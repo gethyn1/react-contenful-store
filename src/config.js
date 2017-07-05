8757a0ed38de6193e95b1df7bcec7323afa201df
Gethyn Jones <gethyn1@gmail.com>
2017-07-05 09:18:33 +0100

    Add dev config for dev deployment

---
+++ src/config.js
@@ -0,0 +1,9 @@
+// @flow
+export const WDS_PORT = 8080
+
+export const APP_NAME = 'React Store'
+
+export const CONTENTFUL_SPACE = 'ffwq3tgojks7'
+export const CONTENTFUL_ACCESS_TOKEN = 'b8551de1e56ea5515008fd0328fd3e670076790273224aec1830897b31940d7f'
+
+export const isProd = process.env.NODE_ENV === 'production'
